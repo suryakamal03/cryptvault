eaf4a9d1c7585fc5f681fd6e0e66342405fbed83
suryakamal03 <professionalsurya3@gmail.com>
2025-08-22 07:00:25 +0530

    maintained

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -492,7 +492,7 @@ function App() {
 
       {/* Login Modal */}
       {hasVault && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
+        <div className="fixed inset-0  bg-opacity-50 backdrop-blur-sm flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-6 w-96 max-w-md border border-gray-300">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-bold">Login to Your Vault</h2>
